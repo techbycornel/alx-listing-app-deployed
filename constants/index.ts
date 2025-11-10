f1454946e6529a852cff8db0089893593aa1d23e
Authcornelius <corneliusedos@gmail.com>
2025-11-10 03:38:09 +0100

    updated

--- constants/index.ts
+++ constants/index.ts
@@ -10,7 +10,8 @@ export const PROPERTYLISTINGSAMPLE: PropertyProps[] = [
     price: 3200,
     offers: { bed: "3", shower: "3", occupants: "4-6" },
     image: "/assets/images/image1.jpg",
-    discount: ""
+    discount: "",
+    id: "1"
   },
   {
     name: "Mountain Escape Chalet",
@@ -20,8 +21,7 @@ export const PROPERTYLISTINGSAMPLE: PropertyProps[] = [
     price: 1800,
     offers: { bed: "4", shower: "2", occupants: "5-7" },
     image: "/assets/images/image2.jpg",
-    discount: "30"
+    discount: "30",
+    id: "2"
   },
-  // ... continue with the rest of the 20 objects (use same pattern)
-  // For brevity paste the rest following the pattern you provided and set image paths
 ];
